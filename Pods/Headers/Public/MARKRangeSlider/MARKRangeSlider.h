b3995393d75549680265014f1cf64a7c23a19819
hanif <hanifhussain@Hanif-PC.home>
2017-03-17 23:00:36 +0000

    Tweaks to ui layout and under the hood improvements

--- Pods/Headers/Public/MARKRangeSlider/MARKRangeSlider.h
+++
@@ -1,1 +0,0 @@
-../../../MARKRangeSlider/Source/MARKRangeSlider.h
---
+++ Pods/Target Support Files/YIPopupTextView/YIPopupTextView-umbrella.h
@@ -0,0 +1,18 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "YIPopupTextView.h"
+#import "YISSTextView.h"
+
+FOUNDATION_EXPORT double YIPopupTextViewVersionNumber;
+FOUNDATION_EXPORT const unsigned char YIPopupTextViewVersionString[];
+
